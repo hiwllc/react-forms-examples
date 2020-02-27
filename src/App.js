fa24adf3a6539fd4f19ceac302751787b23b0004
Wallace Batista <wallacebatistaoliveira@gmail.com>
2020-02-27 13:08:05 -0300

    app: add form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import React from 'react'
 
+import './style.css'
+
 function App() {
-  return <span>Hello World!</span>
+  return (
+    <div className="container">
+      <div className="papper">
+        <h1 className="papper-title">Sign In</h1>
+
+        <form method="post">
+          <div className="field">
+            <label className="field-label" htmlFor="username">
+              username:
+            </label>
+            <input
+              id="username"
+              className="field-input"
+              type="text"
+              name="username"
+            />
+          </div>
+
+          <div className="field">
+            <label className="field-label" htmlFor="email">
+              email:
+            </label>
+            <input
+              id="email"
+              className="field-input"
+              type="email"
+              name="email"
+            />
+          </div>
+
+          <div className="field">
+            <label className="field-label" htmlFor="password">
+              password:
+            </label>
+            <input
+              id="password"
+              className="field-input"
+              type="password"
+              name="password"
+            />
+          </div>
+
+          <div className="field">
+            <label className="field-label check">
+              <input className="field-input" type="checkbox" name="remember" />
+              <span>Remember-me</span>
+            </label>
+          </div>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
